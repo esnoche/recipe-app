28d47b6ce3f3b1cfafe4773e03dc12b5eb71e4f3
satyam <satyam@gmail.com>
2023-09-14 14:28:09 +0530

    added logout button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,18 @@
+import { useCookies } from "react-cookie";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-    return(
+
+    const [cookies, setCookies] = useCookies(["access_token"]);
+
+    return (
         <>
-        <div className="navbar">
-            <Link to="/">Home</Link>
-            <Link to="/create-recipe">Create Recipe</Link>
-            <Link to="/saved-recipes">Saved Recipes</Link>
-            <Link to="/auth">Login/Register</Link>
-        </div>
+            <div className="navbar">
+                <Link to="/">Home</Link>
+                <Link to="/create-recipe">Create Recipe</Link>
+                <Link to="/saved-recipes">Saved Recipes</Link>
+                {!cookies.access_token ? (<Link to="/auth">Login/Register</Link>) : <button>Logout</button>}
+            </div>
         </>
     )
 }
