5274e06c2879a4c0eaafbcf148a99961880d5d0b
satyam <satyam@gmail.com>
2023-09-21 18:03:27 +0530

    working state: getting recipes from db

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [recipes, setRecipes] = useState([]);
+  useEffect(() => {
+    const fetchRecipe = async ()=>{
+      try{
+        const load = await axios.get("http://localhost:3001/recipes");
+        setRecipes(load.data);
+        console.log(load.data);
+      }catch(error){
+        console.error(error);
+      }
+    }
+    fetchRecipe();
+  },[])
   return (
     <div>Home</div>
   )
